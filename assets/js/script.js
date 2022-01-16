36ce75ee378af001f3953fc49fe14610df3b2541
cynogriffin <89767958+cynogriffin@users.noreply.github.com>
2022-01-15 22:44:57 -0600

    added functionality with long lat; added color codes for UV index levels

--- assets/js/script.js
+++ assets/js/script.js
@@ -46,54 +46,70 @@ const app = {
       console.error(err);
     },
     showWeather: (resp) => {
-      console.log(resp);
-      let row = document.querySelector('.weather.row');
-      //clear out the old weather and add the new
-      // row.innerHTML = '';
-      row.innerHTML = resp.daily
-        .map((day, idx) => {
-          if (idx <= 4) {
-            let dt = new Date(day.dt * 1000); //timestamp * 1000
-            let sr = new Date(day.sunrise * 1000).toTimeString();
-            let ss = new Date(day.sunset * 1000).toTimeString();
-            return `<div class="col">
-                <div class="card">
-                <h5 class="card-title p-2">${dt.toDateString()}</h5>
-                  <img
-                    src="http://openweathermap.org/img/wn/${
-                      day.weather[0].icon
-                    }@4x.png"
-                    class="card-img-top"
-                    alt="${day.weather[0].description}"
-                  />
-                  <div class="card-body">
-                    <h3 class="card-title">${day.weather[0].main}</h3>
-                    <p class="card-text">High ${day.temp.max}&deg;F Low ${
-              day.temp.min
-            }&deg;F</p>
-                    <p class="card-text">High Feels like ${
-                      day.feels_like.day
-                    }&deg;F</p>
-                    <p class="card-text">Pressure ${day.pressure}mb</p>
-                    <p class="card-text">Humidity ${day.humidity}%</p>
-                    <p class="card-text">UV Index ${day.uvi}</p>
-                    <p class="card-text">Precipitation ${day.pop * 100}%</p>
-                    <p class="card-text">Dewpoint ${day.dew_point}</p>
-                    <p class="card-text">Wind ${day.wind_speed}m/s, ${
-              day.wind_deg
-            }&deg;</p>
-                    <p class="card-text">Sunrise ${sr}</p>
-                    <p class="card-text">Sunset ${ss}</p>
-                  </div>
+        console.log(resp);
+        let row = document.querySelector('.weather.row');
+        //clear out the old weather and add the new
+        // row.innerHTML = '';
+        row.innerHTML = resp.daily
+            .map((day, idx) => {
+            if (idx <= 4) {
+                let dt = new Date(day.dt * 1000); //timestamp * 1000
+                let sr = new Date(day.sunrise * 1000).toTimeString();
+                let ss = new Date(day.sunset * 1000).toTimeString();
+                
+                return `<div class="col">
+                    <div class="card">
+                    <h5 class="card-title p-2">${dt.toDateString()}</h5>
+                    <img
+                        src="http://openweathermap.org/img/wn/${
+                        day.weather[0].icon
+                        }@4x.png"
+                        class="card-img-top"
+                        alt="${day.weather[0].description}"
+                    />
+                    <div class="card-body">
+                        <h3 class="card-title">${day.weather[0].main}</h3>
+                        <p class="card-text">High ${day.temp.max}&deg;F Low ${
+                day.temp.min
+                }&deg;F</p>
+                        <p class="card-text">High Feels like ${
+                        day.feels_like.day
+                        }&deg;F</p>
+                        <p class="card-text">Pressure ${day.pressure}mb</p>
+                        <p class="card-text">Humidity ${day.humidity}%</p>
+                        <p class="card-text uvcolor">UV Index ${day.uvi}</p>
+                        <p class="card-text">Precipitation ${day.pop * 100}%</p>
+                        <p class="card-text">Dewpoint ${day.dew_point}</p>
+                        <p class="card-text">Wind ${day.wind_speed}m/s, ${
+                day.wind_deg
+                }&deg;</p>
+                        <p class="card-text">Sunrise ${sr}</p>
+                        <p class="card-text">Sunset ${ss}</p>
+                    </div>
+                    </div>
                 </div>
-              </div>
-            </div>`;
-          }
+                </div>`;
+            }
         })
         .join(' ');
+
+        resp.daily.map((day, idx) => {
+            if (idx <= 4) {
+                if(day.uvi <= 2){
+                    $(".uvcolor").addClass("text-success")
+                } else if (day.uvi <= 5) {
+                    $(".uvcolor").addClass("text-warning")
+                } else {
+                    $(".uvcolor").addClass("text-danger")
+                }
+            }
+        });
     },
-  };
+
+};
   
+
+
   app.init();
 
 // var init= function() {
